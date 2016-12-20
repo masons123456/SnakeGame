970191a6f8246afb36010c69cdb49a646341dd20
Mason Davis <masons.davis1998@gmail.com>
2016-12-19 19:33:13 -0600

    Finished OptionsManager

--- src/main/java/com/elematrixstudios/snakegame/SnakeGame.java
+++ src/main/java/com/elematrixstudios/snakegame/SnakeGame.java
@@ -3,13 +3,18 @@ package com.elematrixstudios.snakegame;
 import com.elematrixstudios.snakegame.options.OptionsManager;
 import lombok.Getter;
 
+import java.util.logging.Logger;
+
 public class SnakeGame {
 
     @Getter
     private OptionsManager optionsManager;
+    @Getter
+    private Logger logger;
 
     public SnakeGame() {
-        this.optionsManager = new OptionsManager();
+        this.optionsManager = new OptionsManager(this);
+        this.logger = Logger.getLogger(this.getClass().getName());
     }
 
     public static void main(String[] args) {
--- src/main/java/com/elematrixstudios/snakegame/options/OptionsManager.java
+++ src/main/java/com/elematrixstudios/snakegame/options/OptionsManager.java
@@ -1,5 +1,6 @@
 package com.elematrixstudios.snakegame.options;
 
+import com.elematrixstudios.snakegame.SnakeGame;
 import org.yaml.snakeyaml.Yaml;
 
 import java.awt.Color;
@@ -7,9 +8,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 
 public class OptionsManager {
+
+    private SnakeGame main;
 
     public static int DEFAULT_SNAKE_LENGTH;
     public static Color DEFAULT_SNAKE_COLOR;
@@ -17,10 +21,12 @@ public class OptionsManager {
     private Yaml configFile;
     private HashMap<String, Object> config;
 
-    public OptionsManager() {
+    public OptionsManager(SnakeGame main) {
+        this.main = main;
+
         configFile = new Yaml();
 
-        File f = new File("config.yaml");
+        File f = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "config.yaml");
         InputStream in = null;
         try {
             in = new FileInputStream(f);
@@ -32,8 +38,22 @@ public class OptionsManager {
         this.loadDefaults();
     }
 
+
     private void loadDefaults() {
-        DEFAULT_SNAKE_LENGTH = Integer.parseInt(this.config.get("defaults.snake-length").toString());
-        DEFAULT_SNAKE_COLOR = Color.getColor(this.config.get("defaults.snake-color").toString());
+        if (config == null) {
+            this.main.getLogger().severe("Couldn't load config file! Using hard-defaults!");
+            DEFAULT_SNAKE_LENGTH = 3;
+            DEFAULT_SNAKE_COLOR = Color.GREEN;
+
+        } else {
+            HashMap<String, Object> snakeDefaults = (HashMap<String, Object>) this.config.get("defaults");
+            DEFAULT_SNAKE_LENGTH = Integer.parseInt(snakeDefaults.get("snake-length").toString());
+            try {
+                Field field = Color.class.getField(snakeDefaults.get("snake-color").toString());
+                DEFAULT_SNAKE_COLOR = (Color) field.get(null);
+            } catch (Exception ex) {
+                DEFAULT_SNAKE_COLOR = Color.GREEN;
+            }
+        }
     }
 }
