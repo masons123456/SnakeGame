65ece99f067643492d57f2cf7f62f7aaabd0f8ac
Mason Davis <masons.davis1998@gmail.com>
2016-12-19 19:05:11 -0600

    Basics of Main Class

--- src/main/java/com/elematrixstudios/snakegame/SnakeGame.java
+++ src/main/java/com/elematrixstudios/snakegame/SnakeGame.java
@@ -1,4 +1,18 @@
 package com.elematrixstudios.snakegame;
 
+import com.elematrixstudios.snakegame.options.OptionsManager;
+import lombok.Getter;
+
 public class SnakeGame {
+
+    @Getter
+    private OptionsManager optionsManager;
+
+    public SnakeGame() {
+        this.optionsManager = new OptionsManager();
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame();
+    }
 }
