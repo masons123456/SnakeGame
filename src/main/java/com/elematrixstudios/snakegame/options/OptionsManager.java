bb03c923a91597532cf9c558dbb7b2891c575919
Mason Davis <masons.davis1998@gmail.com>
2016-12-19 19:04:07 -0600

    Created config file. Finished OptionsManager

--- src/main/java/com/elematrixstudios/snakegame/options/OptionsManager.java
+++ src/main/java/com/elematrixstudios/snakegame/options/OptionsManager.java
@@ -1,13 +1,39 @@
 package com.elematrixstudios.snakegame.options;
 
+import org.yaml.snakeyaml.Yaml;
+
 import java.awt.Color;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class OptionsManager {
 
     public static int DEFAULT_SNAKE_LENGTH;
     public static Color DEFAULT_SNAKE_COLOR;
 
+    private Yaml configFile;
+    private HashMap<String, Object> config;
+
     public OptionsManager() {
-        DEFAULT_SNAKE_LENGTH = 3;
+        configFile = new Yaml();
+
+        File f = new File("config.yaml");
+        InputStream in = null;
+        try {
+            in = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.config = (HashMap<String, Object>) configFile.load(in);
+
+        this.loadDefaults();
+    }
+
+    private void loadDefaults() {
+        DEFAULT_SNAKE_LENGTH = Integer.parseInt(this.config.get("defaults.snake-length").toString());
+        DEFAULT_SNAKE_COLOR = Color.getColor(this.config.get("defaults.snake-color").toString());
     }
 }
